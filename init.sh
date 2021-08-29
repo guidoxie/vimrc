b74424e59627362a79700252885e943e6b1bb944
xgh <duanhunyiye@163.com>
2021-08-29 15:32:24 +0800

    download vim-plug

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MYDIR=$(cd $(dirname $0)/; pwd)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 \rm -f ~/.vimrc
 ln -s ${MYDIR}/vimrc ~/.vimrc
 
